29e057e07147a17acf84b2a18df70bdfd702a062
lann <github-lann@lannbox.com>
2011-05-11 10:23:47 -0700

    Edited json2csv.py via GitHub

--- json2csv.py
+++ json2csv.py
@@ -4,15 +4,16 @@ import json
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print """
-Usage:
-  Takes a file with a JSON dictionary on each line and extracts keys to a tab seperated CSV output
-t     ex:
-      $ cat addresses.json | json2csv.py id email_address tracking_id
-      123	user@example.com	XYZ
+        from textwrap import dedent
+        print dedent("""
+            Usage:
+              Takes a file with a JSON dictionary on each line and extracts keys to a tab seperated CSV output
+                  ex:
+                  $ cat addresses.json | json2csv.py id email_address tracking_id
+                  123	user@example.com	XYZ
 
-      $ json2csv.py field1 email id < input.json > output.csv
-"""
+                  $ json2csv.py field1 email id < input.json > output.csv
+            """)
         sys.exit(-1)
 
     for line in sys.stdin:
