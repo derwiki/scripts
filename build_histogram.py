e400237630acc4644d9d6e5c2b3437b6e1b4963e
lann <lann@lannbox.com>
2011-05-13 17:42:47 -0600

    Clean up build_histogram

--- build_histogram.py
+++ build_histogram.py
@@ -2,40 +2,25 @@
 
 import collections
 import datetime
-import os
-import pprint
 import sys
+import time
 
-def bucket_log(log_filename, date_format=None, power_of_ten=None):
-    date_format = date_format or '%Y-%m-%d %H:%M:%S'
-    power_of_ten = power_of_ten or 1
-    def date_string_to_bucket(date_string):
-        date = datetime.datetime.strptime(date_string.strip(), date_format)
-        # break into 10 minute buckets by stripping minutes digit
-        #TODO this only works for values in (1, 2)
-        return date.strftime('%Y-%m-%d %H:%M')[:-1 * power_of_ten] + ('0' * power_of_ten)
-
+def bucket_log(log_filename, date_format='%Y-%m-%d %H:%M:%S', bucket_seconds=10):
+    bucket_seconds = int(bucket_seconds)
+    
     buckets = collections.defaultdict(int)
     with open(log_filename) as logfile:
         for line in logfile:
-            buckets[date_string_to_bucket(line)] += 1
+            ts = time.mktime(
+                time.strptime(line, date_format))
+            buckets[ts // bucket_seconds] += 1
 
-    for event_datetime, count in sorted(buckets.items()):
-        print "[%s] %s" % (event_datetime, count)
-    return
+    for ts, count in sorted(buckets.items()):
+        bucket = datetime.datetime.fromtimestamp(ts * bucket_seconds)
+        print "[%s] %s" % (bucket.strftime(date_format), count)
 
 if __name__ == '__main__':
-    data_filename = sys.argv[1]
-    date_format = None
-    assert os.path.exists(data_filename)
-
-    if len(sys.argv) > 2:
-        date_format = sys.argv[2]
-        try:
-            datetime.datetime.now().strftime(date_format)
-        except:
-            raise Exception("Invalid datetime format: %s" % date_format)
-
-    power_of_ten = int(sys.argv[3]) if len(sys.argv) > 3 else None
-
-    print bucket_log(data_filename, date_format, power_of_ten)
+    try:
+        bucket_log(*sys.argv[1:4])
+    except IOError, e:
+        print 'Error reading file: %s' % e
