ecfa56d45f22356fe0c3a71fd484fea84577ba70
Adam Derewecki <adam@causes.com>
2011-05-13 17:21:56 -0700

    Added generate_email_chunk.sh This script is probably only useful for me; it concatenates email recipient lists and a seed list of JSON

---
+++ generate_email_chunk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This is probably not useful for anyone else
+
+indices=$(seq -f %003.0f -s'_' $1 $2)
+filename=${3}${indices}_seed.json
+indices=$(seq -f %003.0f $1 $2)
+
+if [ -e $filename ]; then
+  echo File $filename already exists, please remove file and try again
+  exit -1
+fi
+
+echo Using indices: $indices
+echo Writing to file: $filename
+
+for i in $indices; do
+  filename_index=$3$i.json.gz
+  echo Reading ${filename_index}...
+  zcat $filename_index | sed 's/field1/anonymous_id/g' | sed 's/anonid/anonymous_id/g' >> $filename
+done
+
+cat causes_seedlist.json >> $filename
+
+echo Generated file length: $(wc -l $filename | cut -d' ' -f 1)
+echo Sample of generated file:
+head -n 3 $filename
+tail -n 3 $filename
+echo
