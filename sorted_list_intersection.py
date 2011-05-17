66bab33f2ed1b612b3f9c50928a2aa4eecf4a362
Adam Derewecki <derewecki@gmail.com>
2011-05-17 09:22:26 -0700

    Added basic benchmarks to sorted_list_intersection

--- sorted_list_intersection.py
+++ sorted_list_intersection.py
@@ -11,9 +11,32 @@ def sorted_intersection(list_a, list_b):
   except IndexError:
     return intersection
 
+test_variables = (
+  (50000, 25000, 50000, 25000),
+  (500000, 250000, 50000, 25000),
+  (5000000, 2500000, 50000, 25000),
+  (list_50m, _, 500, 250),
+  (list_50m, _, 5000, 2500),
+  (list_5om, _, 50000, 25000),
+)
+
+
 if __name__ == '__main__':
-    list_a = range(10)
-    list_b = range(1, 15, 2)
-    print list_a
-    print list_b
-    print sorted_intersection(list_a, list_b)
+    import time, random
+    list_50m = random.sample(range(35000000), 25000000)
+    for a, a_choose, b, b_choose in test_variables:
+        list_a = random.sample(range(a), a_choose) if a is not list_50m else list_50m
+        set_a = set(list_a)
+        sorted_a = sorted(list_a)
+        list_b = random.sample(range(b), b_choose)
+        hash_start = time.time()
+        hash_res = set_a.intersection(list_b)
+        hash_end = time.time()
+        print "hash: list_a %d, list_b %d, res %d: time %f" % (len(list_a), len(list_b), len(hash_res), hash_end - hash_start)
+
+        list_start = time.time()
+        list_res = sorted_intersection(sorted_a, sorted(list_b))
+        list_end = time.time()
+        print "list: list_a %d, list_b %d, res %d: time %f" % (len(list_a), len(list_b), len(list_res), list_end - list_start)
+        print '=' * 40
+
