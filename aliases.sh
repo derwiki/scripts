6aaa7a5d3b784f5698c02331821f1f42eba9fa22
Adam Derewecki <derewecki@gmail.com>
2011-05-22 22:15:52 -0700

    Attempted to add ssl_encrypt and ssl_decrypt aliases. Untested in Linux and not working in OS X though...

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,5 @@
 alias us_email_only_grep="grep -e '(.org\|.com\|.net\|.edu\|.mil\|.gov)'"
+
+# these used to work on linux but might not on OS X
+alias ssl_encrypt="openssl des3 -salt -in $1 -out > $1.encrypted"
+alias ssl_decrypt="openssl des3 -d -salt -in file -out $2 -k $1"
