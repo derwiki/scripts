da73ffb6c2074ecc3597527e2c701bdfe86b25bf
Adam Derewecki <derewecki@gmail.com>
2011-06-16 01:00:30 -0700

    Commented out route in server.py and added comment

--- server.py
+++ server.py
@@ -10,13 +10,14 @@ bottle.debug(True)
 log = logging.getLogger('servlet')
 log.setLevel(logging.DEBUG)
 
-@route('/play')
-def play():
-    encoded_path = request.GET.get('path', '')
-    path = urllib.pathname2url(encoded_path)
-    if not os.path.exists(path): # False on empty string
-        return dict(success=False, reason="Path %s does not exist" % path)
-    return dict(success=True)
+# Example route:
+#@route('/play')
+#def play():
+#    encoded_path = request.GET.get('path', '')
+#    path = urllib.pathname2url(encoded_path)
+#    if not os.path.exists(path): # False on empty string
+#        return dict(success=False, reason="Path %s does not exist" % path)
+#    return dict(success=True)
 
 
 port = sys.argv[1] if len(sys.argv) > 1 else 8080
