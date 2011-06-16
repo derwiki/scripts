6b25757a60b06bca2f881c61fcf324c7899cff2c
Adam Derewecki <adam@mainframe.corp.causes.com>
2011-06-16 02:01:43 -0700

    Added bootstrap and added git alias to aliases.sh
    bootstrap is a script that you can download with wget/curl that will
    clone my scripts and dotfiles repos, link to the dotfiles, download
    and compile the latest git source code, and adds aliases.sh to the
    user's .profile
    
    The git alias is simply to override the git that is found on $PATH
    (suggestions on a better way to do this are welcome)

--- aliases.sh
+++ aliases.sh
@@ -1,9 +1,8 @@
 alias us_email_only_grep="grep -e '(.org\|.com\|.net\|.edu\|.mil\|.gov)'"
 
-# these used to work on linux but might not on OS X
+# these were tested on Linux but might not on OS X
 alias ssl_encrypt="openssl des3 -salt -in $1 -out > $1.encrypted"
 alias ssl_decrypt="openssl des3 -d -salt -in file -out $2 -k $1"
-
 
 alias pf="grep -nr --include=*.py"
 alias jsf="grep -nr --include=*.js"
@@ -12,3 +11,4 @@ alias ef="grep -nr --include=*.erb"
 alias rkf="grep -nr --include=*.rake"
 
 alias s="screen -xRR"
+alias git="$HOME/bin/git"
