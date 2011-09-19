06027c403cece5047c4106a6250df60bbd464500
Adam Derewecki <derewecki@gmail.com>
2011-09-19 01:15:03 -0700

    Add ssh-reagent
    Source:
      http://tychoish.com/rhizome/9-awesome-ssh-tricks/

--- helpers.sh
+++ helpers.sh
@@ -63,3 +63,15 @@ function refactor {
 function pid_seconds {
   echo $(($(date +%s) - $(stat -c '%Y' /proc/$1)))
 }
+
+ssh-reagent () {
+  for agent in /tmp/ssh-*/agent.*; do
+    export SSH_AUTH_SOCK=$agent
+    if ssh-add -l 2>&1 > /dev/null; then
+      echo Found working SSH Agent:
+      ssh-add -l
+      return
+    fi
+  done
+  echo Cannot find ssh agent - maybe you should reconnect and forward it?
+}
